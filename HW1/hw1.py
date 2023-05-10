f3651b060c720901339ed361ad647e209a154b6b
sam <gujjulassr1997@gmail.com>
2023-05-10 11:34:30 +0530

    add the code with using much loops

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -17,18 +17,28 @@ NA=0 #number of possiblities for getting same number
 NB=0 #number of posiibilities for getting different number
 
 SampleSpace=6**2 # Total possibilites for two dice when they are rolled simultaneously
+dice1={}
+dice2={}
+for i in range(1,7):
+    if i in dice1:
+        dice1[i]=dice1[i]+1
+    else:
+        dice1[i]=1
+    if i in dice2:
+        dice2[i]=dice2[i]+1
+    else:
+        dice2[i]=1
 
-for firstDice in range(1,7):
-    for secondDice in range (1,7):
-
-        if firstDice==secondDice:
-            NA=NA+1
-        else:
-            NB=NB+1
-
+for key in dice1:
+    if key in dice2:
+        NA=NA+dice1[key]*dice2[key]
 PA=NA/SampleSpace
-PB=NB/SampleSpace
+
+PB=(len(dice1)*len(dice2)-NA)/SampleSpace #NB can be calculate total number of possibilities -possible outcomes for Event A as they were mutually exclusive
+
+
 
 print("Probability of getting same number ",PA)
 print("Probability of getting different number ",PB)
+
 
